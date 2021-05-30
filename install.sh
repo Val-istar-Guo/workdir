4d210b1ed4c00f58e3cbf060992e9cdf5b51ad7e
val.istar.guo <val.istar.guo@gmail.com>
2021-05-30 22:38:18 +0800

    Initial commit

---
+++ install.sh
@@ -0,0 +1,9 @@
+[ ! -d @mili/mili ] && git clone git@github.com:mili-project-manager/mili.git @mili/mili
+[ ! -d @mtpl/template ] && git clone git@github.com:mili-project-manager/mtpl-template.git @mtpl/template
+[ ! -d @mtpl/github ] && git clone git@github.com:mili-project-manager/mtpl-github.git @mtpl/github
+[ ! -d @mtpl/conventional-commits ] && git clone git@github.com:mili-project-manager/mtpl-conventional-commits.git @mtpl/conventional-commits
+[ ! -d @mtpl/base ] && git clone git@github.com:mili-project-manager/mtpl-base.git @mtpl/base
+[ ! -d @mtpl/component ] && git clone git@github.com:mili-project-manager/mtpl-component.git @mtpl/component
+[ ! -d @mtpl/nestjs ] && git clone git@github.com:mili-project-manager/mtpl-nestjs.git @mtpl/nestjs
+[ ! -d @mtpl/code-style ] && git clone git@github.com:mili-project-manager/mtpl-code-style.git @mtpl/code-style
+
